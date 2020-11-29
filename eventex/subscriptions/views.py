f235fed12cc51898e9c3fa56e49ea2ae849c15b4
newmarwegner <newmarwegner@gmail.com>
2020-11-29 17:59:37 -0300

    email

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -14,8 +14,8 @@ def subscribe(request):
             body=render_to_string('subscriptions/subscription_email.txt',form.cleaned_data)
             mail.send_mail('Confirmação de inscrição',
                            body,
-                           'contato@eventex.com.br',
-                           ['contato@eventex.com.br', form.cleaned_data['email']])
+                           'newmarwegner@gmail.com',
+                           ['newmarwegner@gmail.com', form.cleaned_data['email']])
             messages.success(request,'Inscrição realizada com sucesso!')
             return HttpResponseRedirect('/inscricao/')
         else:
