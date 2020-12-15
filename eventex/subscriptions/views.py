dda4f8adc18fb44063d29d76f35ae86137680a9c
newmarwegner <newmarwegner@gmail.com>
2020-12-15 10:05:03 -0300

    Revert "email"
    This reverts commit f235fed12cc51898e9c3fa56e49ea2ae849c15b4.

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -14,8 +14,8 @@ def subscribe(request):
             body=render_to_string('subscriptions/subscription_email.txt',form.cleaned_data)
             mail.send_mail('Confirmação de inscrição',
                            body,
-                           'newmarwegner@gmail.com',
-                           ['newmarwegner@gmail.com', form.cleaned_data['email']])
+                           'contato@eventex.com.br',
+                           ['contato@eventex.com.br', form.cleaned_data['email']])
             messages.success(request,'Inscrição realizada com sucesso!')
             return HttpResponseRedirect('/inscricao/')
         else:
